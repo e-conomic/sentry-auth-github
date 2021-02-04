5e4dd4827c2f4d85ba1955610bc1079c3bbd6703
Calin Laurentiu Ilie <calinilielaur@gmail.com>
2021-02-04 11:06:06 +0100

    EC-28940 Github auth moved to headers

--- sentry_auth_github/client.py
+++ sentry_auth_github/client.py
@@ -9,51 +9,41 @@ from .constants import API_DOMAIN
 
 
 class GitHubApiError(Exception):
-    def __init__(self, message='', status=0):
+    def __init__(self, message="", status=0):
         super(GitHubApiError, self).__init__(message)
         self.status = status
 
 
 class GitHubClient(object):
-    def __init__(self, client_id, client_secret):
-        self.client_id = client_id
-        self.client_secret = client_secret
+    def __init__(self, access_token):
         self.http = http.build_session()
+        self.access_token = access_token
 
-    def _request(self, path, access_token):
-        params = {
-            'client_id': self.client_id,
-            'client_secret': self.client_secret,
-        }
-
-        headers = {
-            'Authorization': 'token {0}'.format(access_token),
-        }
+    def _request(self, path):
+        headers = {"Authorization": "token {0}".format(self.access_token)}
 
         try:
-            req = self.http.get('https://{0}/{1}'.format(API_DOMAIN, path.lstrip('/')),
-                params=params,
-                headers=headers,
+            req = self.http.get(
+                "https://{0}/{1}".format(API_DOMAIN, path.lstrip("/")), headers=headers,
             )
         except RequestException as e:
-            raise GitHubApiError(six.text_type(e), status=getattr(e, 'status_code', 0))
+            raise GitHubApiError(six.text_type(e), status=getattr(e, "status_code", 0))
         if req.status_code < 200 or req.status_code >= 300:
             raise GitHubApiError(req.content, status=req.status_code)
         return json.loads(req.content)
 
-    def get_org_list(self, access_token):
-        return self._request('/user/orgs', access_token)
+    def get_org_list(self):
+        return self._request("/user/orgs")
 
-    def get_user(self, access_token):
-        return self._request('/user', access_token)
+    def get_user(self):
+        return self._request("/user")
 
-    def get_user_emails(self, access_token):
-        return self._request('/user/emails', access_token)
+    def get_user_emails(self):
+        return self._request("/user/emails")
 
-    def is_org_member(self, access_token, org_id):
-        org_list = self.get_org_list(access_token)
+    def is_org_member(self, org_id):
         org_id = six.text_type(org_id)
-        for o in org_list:
-            if six.text_type((o['id'])) == org_id:
+        for o in self.get_org_list():
+            if six.text_type((o["id"])) == org_id:
                 return True
         return False
